598502c439c0935156ff4a022a2ffc7814dd9895
anderson ryan <andersonryan.2015.ar@gmail.com>
2022-10-26 11:41:05 -0300

    implementando butones

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@ const pokemonImg = document.querySelector('.pokemon_img');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
+const btn_voltar = document.querySelector('.btn-voltar');
+const btn_proximo = document.querySelector('.btn-proximo');
+
+let buscarPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
-    const apiRes = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    const apiRes = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     
     if (apiRes.status === 200) {
         const data = await apiRes.json();
@@ -22,13 +26,16 @@ const renderPokemon =async (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
     if (data) {
-        pokemonName.innerHTML = data.name;
+        pokemonImg.style.display = 'block';
+        pokemonName.innerHTML = data.name.toLowerCase();
         pokemonNumber.innerHTML = data.id;
         pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
 
         input.value = '';
+        buscarPokemon = data.id;
     }
     else {
+        pokemonImg.style.display = 'none';
         pokemonName.innerHTML = 'Não existe';
         pokemonNumber.innerHTML = '';
     }
@@ -42,4 +49,16 @@ form.addEventListener('submit', (event) => {
     
 });
 
-renderPokemon('1')
+btn_voltar.addEventListener('click', () => {
+    if (buscarPokemon > 1) {
+        buscarPokemon -= 1;
+        renderPokemon(buscarPokemon)
+    }
+});
+
+btn_proximo.addEventListener('click', () => {
+    buscarPokemon += 1;
+    renderPokemon(buscarPokemon)
+});
+
+renderPokemon(buscarPokemon)
