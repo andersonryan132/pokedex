504b592904b8db975a371e3676e3f641c35e351d
anderson ryan <andersonryan.2015.ar@gmail.com>
2022-10-26 11:26:02 -0300

    tratação de erros

--- js/script.js
+++ js/script.js
@@ -7,19 +7,31 @@ const input = document.querySelector('.input_search');
 
 const fetchPokemon = async (pokemon) => {
     const apiRes = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
-    const data = await apiRes.json();
+    
+    if (apiRes.status === 200) {
+        const data = await apiRes.json();
 
-    return data;
+        return data;
+    } 
 }
 
 const renderPokemon =async (pokemon) => {
+    pokemonName.innerHTML = 'Carregando...';
+    pokemonNumber.innerHTML = '';
+
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+    if (data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
 
-    input.value = '';
+        input.value = '';
+    }
+    else {
+        pokemonName.innerHTML = 'Não existe';
+        pokemonNumber.innerHTML = '';
+    }
 }
 
 form.addEventListener('submit', (event) => {
@@ -29,3 +41,5 @@ form.addEventListener('submit', (event) => {
     renderPokemon(input.value);
     
 });
+
+renderPokemon('1')
